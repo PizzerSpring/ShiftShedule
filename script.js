a6e45a718dc44eeee0aa0d79ef313a5e715d318c
Axendr <vladi-slav96@mail.ru>
2021-02-01 20:19:45 +0700

    leap
    leapcount

--- script.js
+++ script.js
@@ -1,142 +1,245 @@
 'use strict';
 
-const button = document.querySelector('.menu__but');
+const startString = document.querySelectorAll('.startString');
+const middleString = document.querySelectorAll('.middleString');
+
+const weekmarch2 = document.querySelector('.weakmarch2');
+const tr = document.querySelectorAll('tr');
+
+const weekleap1 = document.querySelector('.weekleap1');
+const weekleap2 = document.querySelector('.weekleap2');
+
+const date = document.querySelectorAll('td.date');
+const addYear = document.querySelector('.addyear');
+
+const mday = document.querySelector('#menu__mday');
+const mmonth = document.querySelector('#menu__mmonth');
+const menuyear = document.querySelector('#menu__myear');
 
 const mshifttypeone = document.querySelector('#menu__shifttypeone');
 const mshifttypetwo = document.querySelector('#menu__shifttypetwo');
 
-const jdate = document.querySelectorAll('td.jdate');
-const fdate = document.querySelectorAll('td.fdate');
-const mrdate = document.querySelectorAll('td.mrdate');
+const button = document.querySelector('.menu__but');
+const buttonClear = document.querySelector('.menu__buttonclear');
 
-const mday = document.querySelector('#menu__mday');
-const mmonth = document.querySelector('#menu__mmonth');
 
-mmonth.onchange = () => {
-    for (let i = 0; i < jdate.length; i++) {
-        jdate[i].classList.remove('menu_butcol');
+/*Button for clear */
+
+buttonClear.onclick = () => {
+    for (let i = 0; i < date.length; i++) {
+        date[i].classList.remove('menu_butcol');
     }
-}
+};
 
-mday.onchange = () => {
-    for (let i = 0; i < jdate.length; i++) {
-        jdate[i].classList.remove('menu_butcol');
-    }
-}
-
-mshifttypeone.onchange = () => {
-    for (let i = 0; i < jdate.length; i++) {
-        jdate[i].classList.remove('menu_butcol');
-    }
-}
-
-mshifttypetwo.onchange = () => {
-    for (let i = 0; i < jdate.length; i++) {
-        jdate[i].classList.remove('menu_butcol');
-    }
-}
-
-function oneone() {
-
-}
-
-function fivetwo(svalue, mdate) {
-    for (let i = svalue; i < mdate.length; i++) {
-        mdate[i].classList.add('menu_butcol');
-        mdate[i - 1].classList.add('menu_butcol');
-        mdate[i + 1].classList.add('menu_butcol');
-        mdate[i + 2].classList.add('menu_butcol');
-        mdate[i + 3].classList.add('menu_butcol');
-        i = i + 6;
-    }
-}
-
-function twotwo(svalue, mdate) {
-    for (let i = svalue; i < mdate.length; i++) {
-        mdate[i].classList.add('menu_butcol');
-        mdate[i - 1].classList.add('menu_butcol');
-        i = i + 3;
-    }
-}
-
-//private int[] mass = { 31, 30 }
-
-let arr = [31,28,31,30,31,30,31,31,30,30,31,31];
+/*Button for count */
 
 button.onclick = () => {
-    const sindex = mday.options.selectedIndex;
-    let startDay = +mday.options[sindex].value;
+    let year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let startDay = +mday.value;
     startDay = startDay - 1;
 
-    let selectedIndexMonth = mmonth.options.selectedIndex;
-    let startMonth = +mmonth.options[selectedIndexMonth].value;
-
-    for (let i = 0; i < startMonth; i++){
-        startDay += arr[i]
+    let startMonth = +mmonth.value;
+    for (let i = 0; i < startMonth; i++) {
+        startDay += year[i];
     }
+
 
     let workDays = +mshifttypeone.value;
     let weekendDays = +mshifttypetwo.value;
     let shiftWorkDays = workDays + weekendDays;
 
-    for (let i = startDay; i < jdate.length; i = i + shiftWorkDays) {
-        for (let j = i; j < workDays + i; j++){
-            if (j < jdate.length)
-                jdate[j].classList.add('menu_butcol');
+    for (let i = startDay; i < date.length; i = i + shiftWorkDays) {
+        for (let j = i; j < workDays + i; j++) {
+            if (j < date.length)
+                date[j].classList.add('menu_butcol');
         }
     }
-
-    /*switch (mmonth.value) {
-        case 'J':
-            if (mshifttypeone.value == '2' && mshifttypetwo.value == '2') {
-                for (let i = svalue; i < jdate.length; i++) {
-                    jdate[i].classList.add('menu_butcol');
-                    jdate[i - 1].classList.add('menu_butcol');
-                    i = i + 3;
-                }
-
-            }
-            if (mshifttypeone.value == '5' && mshifttypetwo.value == '2') {
-                for (let i = svalue; i < jdate.length; i++) {
-                    jdate[i].classList.add('menu_butcol');
-                    jdate[i - 1].classList.add('menu_butcol');
-                    jdate[i + 1].classList.add('menu_butcol');
-                    jdate[i + 2].classList.add('menu_butcol');
-                    jdate[i + 3].classList.add('menu_butcol');
-                    i = i + 6;
-                }
-
-            }
-            break;
-        case 'F':
-            if (mshifttypeone.value == '2' && mshifttypetwo.value == '2') {
-                for (let i = svalue; i < jdate.length; i++) {
-                    jdate[i].classList.add('menu_butcol');
-                    jdate[i - 1].classList.add('menu_butcol');
-                    i = i + 3;
-                }
-
-            }
-            if (mshifttypeone.value == '5' && mshifttypetwo.value == '2') {
-                for (let i = svalue; i < jdate.length; i++) {
-                    jdate[i].classList.add('menu_butcol');
-                    jdate[i - 1].classList.add('menu_butcol');
-                    jdate[i + 1].classList.add('menu_butcol');
-                    jdate[i + 2].classList.add('menu_butcol');
-                    jdate[i + 3].classList.add('menu_butcol');
-                    i = i + 6;
-                }
-
-            }
-            break;
-        case 'Mr':
-            if (mshifttypeone.value == '2' && mshifttypetwo.value == '2') {
-               // twotwo(svalue, mrdate);
-            }
-            if (mshifttypeone.value == '5' && mshifttypetwo.value == '2') {
-              //  fivetwo(svalue, mrdate);
-            }
-            break;
-    }*/
-
 };
+
+//const tde = document.querySelectorAll('td');
+/*Function for new NodeList*/
+function collection() {
+    let datey = document.querySelectorAll('td.date');
+    //for(let i = 0; i < tde.length; i++) {
+    //  tde[85].remove();
+    //console.log(tde);
+    // }
+    return datey;
+}
+
+console.log(middleString[10].lastElementChild);
+
+/*Function change year*/
+
+console.log(startString.length);
+menuyear.onchange = () => {
+
+
+    if (+menuyear.value == 1) {
+
+        let td = document.createElement('td');
+        td.className = 'date';
+        td.innerHTML = '29';
+        addYear.append(td);
+
+        let tdm = document.createElement('td');
+        tdm.innerHTML = ' ';
+
+
+        console.log(tdm);
+
+
+
+
+        // let u = collection()[63];
+        // u.remove();
+        let stringTR = [];
+        for (let i = 0; i < 10; i++) {
+            stringTR[i] = tdm.cloneNode(true);
+        }
+        console.log(stringTR);
+
+        let trans = [];
+
+
+
+
+      /*  for (let i = 0; i < startString.length; i++) {
+            if(i == 1) {
+                break;
+            }
+            startString[i].prepend(stringTR[i]);
+
+            for (let j = 0; j < middleString.length; j++) {
+                if(j == 53) {
+                    break;
+                }
+                if(j == 11) {
+                    startString[2].prepend(stringTR[2]);
+                }
+                if(j == 15) {
+                    startString[3].prepend(stringTR[3]);
+                }
+                if(j == 20) {
+                    startString[4].prepend(stringTR[4]);
+                }
+                if(j == 26) {
+                    startString[5].prepend(stringTR[5]);
+                }
+                if(j == 32) {
+                    startString[6].prepend(stringTR[6]);
+                }
+                if(j == 36) {
+                    startString[7].prepend(stringTR[7]);
+                }
+                if(j == 42) {
+                    startString[8].prepend(stringTR[8]);
+                }
+                if(j == 48) {
+                    startString[9].prepend(stringTR[9]);
+                }
+                if (j != 4 && j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
+                  //  if (j != 9) {
+                        trans[j] = middleString[j].lastElementChild;
+                 //   }
+                    console.log(trans);
+                    console.log(trans[j]);
+                    middleString[j].lastElementChild.remove();
+                    if (j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
+                        middleString[j + 1].prepend(trans[j]);
+                    }
+                }
+                if (j == 5 || j == 43) {
+                    middleString[j].remove();
+                }
+               // console.log(trans[j]);
+            }
+        }*/
+
+
+            for (let j = 0; j < middleString.length; j++) {
+                if(j == 53) {
+                    break;
+                }
+                for(let k = 0; k < 9; k++) {
+                    if(j == 0 || j == 11 || j == 15 || j == 20 || j == 26 || j == 32 || j == 36 || j == 42 || j == 48) {
+                        startString[k].prepend(stringTR[k]);
+                    }
+                }
+                if (j != 4 && j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
+                        trans[j] = middleString[j].lastElementChild;
+                    middleString[j].lastElementChild.remove();
+                    if (j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
+                        middleString[j + 1].prepend(trans[j]);
+                    }
+                }
+                if (j == 5 || j == 43) {
+                    middleString[j].remove();
+                }
+               // console.log(trans[j]);
+            }
+        
+
+        console.log(stringTR.tr1);
+
+
+
+
+        // tr[0].prepend(tdm);
+        //tr[1].prepend(clone);
+        //weekmarch.append(u);
+        // console.log(u);
+        collection();
+        // weekleap1.innerText = 'ВС';
+
+        //weekmarch.prepend(tdm);
+
+        // weekmarch.prepend(tdm);
+        // weekleap2.textContent = 'ПН';
+        //console.log(collection()[59]);
+        //let tdm  = document.createElement('td');
+        //tdm.innerHTML = '';
+        //weekmarch.classList.lastChild.append(tdm);
+
+        button.onclick = () => {
+            let year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            let startDay = +mday.value;
+            startDay = startDay - 1;
+
+            let startMonth = +mmonth.value;
+            for (let i = 0; i < startMonth; i++) {
+                startDay += year[i];
+            }
+
+
+            let workDays = +mshifttypeone.value;
+            let weekendDays = +mshifttypetwo.value;
+            let shiftWorkDays = workDays + weekendDays;
+
+            for (let i = startDay; i < collection().length; i = i + shiftWorkDays) {
+                for (let j = i; j < workDays + i; j++) {
+                    if (j < collection().length)
+                        collection()[j].classList.add('menu_butcol');
+                }
+            }
+            //console.log(collection());
+        }
+
+
+    } else if (+menuyear.value == 0) {
+        /*for (let i = 0; i < datey.length; i++) {
+            datey[59].remove();
+        }*/
+        collection()[59].remove();
+        //console.log(collection());
+        // weekleap1.innerHTML = 'ПН';
+        //weekleap2.innerHTML = 'ВС';
+    }
+};
+
+ //console.log(date);
+
+
+
+
+//console.log(datey);
