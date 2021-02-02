b5b5bb4a03435f054089e59be5a1b892b39b9815
Axendr <vladi-slav96@mail.ru>
2021-02-02 20:25:00 +0700

    flex-wrap

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 'use strict';
 
-const startString = document.querySelectorAll('.startString');
-const middleString = document.querySelectorAll('.middleString');
+/*Getting elements*/
 
-const weekmarch2 = document.querySelector('.weakmarch2');
-const tr = document.querySelectorAll('tr');
-
-const weekleap1 = document.querySelector('.weekleap1');
-const weekleap2 = document.querySelector('.weekleap2');
+const startStringleap = document.querySelectorAll('.startString');
+const middleStringleap = document.querySelectorAll('.middleString');
 
 const date = document.querySelectorAll('td.date');
-const addYear = document.querySelector('.addyear');
+const addLeapdate = document.querySelector('.addLeapdate');
 
-const mday = document.querySelector('#menu__mday');
-const mmonth = document.querySelector('#menu__mmonth');
-const menuyear = document.querySelector('#menu__myear');
+const sday = document.querySelector('#menu__sday');
+const smonth = document.querySelector('#menu__smonth');
+const menusyear = document.querySelector('#menu__syear');
 
 const mshifttypeone = document.querySelector('#menu__shifttypeone');
 const mshifttypetwo = document.querySelector('#menu__shifttypetwo');
 
-const button = document.querySelector('.menu__but');
+const buttonCount = document.querySelector('.menu__count');
 const buttonClear = document.querySelector('.menu__buttonclear');
 
 
@@ -33,12 +29,12 @@ buttonClear.onclick = () => {
 
 /*Button for count */
 
-button.onclick = () => {
+buttonCount.onclick = () => {
     let year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let startDay = +mday.value;
+    let startDay = +sday.value;
     startDay = startDay - 1;
 
-    let startMonth = +mmonth.value;
+    let startMonth = +smonth.value;
     for (let i = 0; i < startMonth; i++) {
         startDay += year[i];
     }
@@ -56,190 +52,77 @@ button.onclick = () => {
     }
 };
 
-//const tde = document.querySelectorAll('td');
-/*Function for new NodeList*/
-function collection() {
+/*Function for leap year*/
+function collectionForLeap() {
     let datey = document.querySelectorAll('td.date');
-    //for(let i = 0; i < tde.length; i++) {
-    //  tde[85].remove();
-    //console.log(tde);
-    // }
     return datey;
 }
 
-console.log(middleString[10].lastElementChild);
-
 /*Function change year*/
 
-console.log(startString.length);
-menuyear.onchange = () => {
+menusyear.onchange = () => {
 
+    if (+menusyear.value == 1) {
 
-    if (+menuyear.value == 1) {
+        let tdAdd = document.createElement('td');
+        tdAdd.className = 'date';
+        tdAdd.innerHTML = '29';
+        addLeapdate.append(tdAdd);
 
-        let td = document.createElement('td');
-        td.className = 'date';
-        td.innerHTML = '29';
-        addYear.append(td);
+        let tdEmpty = document.createElement('td');
+        tdEmpty.innerHTML = ' ';
 
-        let tdm = document.createElement('td');
-        tdm.innerHTML = ' ';
-
-
-        console.log(tdm);
-
-
-
-
-        // let u = collection()[63];
-        // u.remove();
         let stringTR = [];
         for (let i = 0; i < 10; i++) {
-            stringTR[i] = tdm.cloneNode(true);
+            stringTR[i] = tdEmpty.cloneNode(true);
         }
-        console.log(stringTR);
 
         let trans = [];
 
+        /*Rerendering DOM for leap year*/
 
-
-
-      /*  for (let i = 0; i < startString.length; i++) {
-            if(i == 1) {
-                break;
+        for (let j = 0; j < middleStringleap.length - 1; j++) {
+            for(let k = 0; k < 9; k++) {
+                if(j == 0 || j == 11 || j == 15 || j == 20 || j == 26 || j == 32 || j == 36 || j == 42 || j == 48) {
+                    startStringleap[k].prepend(stringTR[k]);
+                }
             }
-            startString[i].prepend(stringTR[i]);
-
-            for (let j = 0; j < middleString.length; j++) {
-                if(j == 53) {
-                    break;
+            if (j != 4 && j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
+                    trans[j] = middleStringleap[j].lastElementChild;
+                middleStringleap[j].lastElementChild.remove();
+                if (j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
+                    middleStringleap[j + 1].prepend(trans[j]);
                 }
-                if(j == 11) {
-                    startString[2].prepend(stringTR[2]);
-                }
-                if(j == 15) {
-                    startString[3].prepend(stringTR[3]);
-                }
-                if(j == 20) {
-                    startString[4].prepend(stringTR[4]);
-                }
-                if(j == 26) {
-                    startString[5].prepend(stringTR[5]);
-                }
-                if(j == 32) {
-                    startString[6].prepend(stringTR[6]);
-                }
-                if(j == 36) {
-                    startString[7].prepend(stringTR[7]);
-                }
-                if(j == 42) {
-                    startString[8].prepend(stringTR[8]);
-                }
-                if(j == 48) {
-                    startString[9].prepend(stringTR[9]);
-                }
-                if (j != 4 && j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
-                  //  if (j != 9) {
-                        trans[j] = middleString[j].lastElementChild;
-                 //   }
-                    console.log(trans);
-                    console.log(trans[j]);
-                    middleString[j].lastElementChild.remove();
-                    if (j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
-                        middleString[j + 1].prepend(trans[j]);
-                    }
-                }
-                if (j == 5 || j == 43) {
-                    middleString[j].remove();
-                }
-               // console.log(trans[j]);
             }
-        }*/
-
-
-            for (let j = 0; j < middleString.length; j++) {
-                if(j == 53) {
-                    break;
-                }
-                for(let k = 0; k < 9; k++) {
-                    if(j == 0 || j == 11 || j == 15 || j == 20 || j == 26 || j == 32 || j == 36 || j == 42 || j == 48) {
-                        startString[k].prepend(stringTR[k]);
-                    }
-                }
-                if (j != 4 && j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
-                        trans[j] = middleString[j].lastElementChild;
-                    middleString[j].lastElementChild.remove();
-                    if (j != 10 && j != 15 && j != 20 && j != 26 && j != 31 && j != 36 && j != 42 && j != 48) {
-                        middleString[j + 1].prepend(trans[j]);
-                    }
-                }
-                if (j == 5 || j == 43) {
-                    middleString[j].remove();
-                }
-               // console.log(trans[j]);
+            if (j == 5 || j == 43) {
+                middleStringleap[j].remove();
             }
-        
-
-        console.log(stringTR.tr1);
-
-
-
-
-        // tr[0].prepend(tdm);
-        //tr[1].prepend(clone);
-        //weekmarch.append(u);
-        // console.log(u);
-        collection();
-        // weekleap1.innerText = 'ВС';
-
-        //weekmarch.prepend(tdm);
-
-        // weekmarch.prepend(tdm);
-        // weekleap2.textContent = 'ПН';
-        //console.log(collection()[59]);
-        //let tdm  = document.createElement('td');
-        //tdm.innerHTML = '';
-        //weekmarch.classList.lastChild.append(tdm);
-
-        button.onclick = () => {
+        }
+            /*Reapeat for leap year*/
+            collectionForLeap();
+        buttonCount.onclick = () => {
             let year = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-            let startDay = +mday.value;
+
+
+            let startDay = +sday.value;
             startDay = startDay - 1;
 
-            let startMonth = +mmonth.value;
+            let startMonth = +smonth.value;
             for (let i = 0; i < startMonth; i++) {
                 startDay += year[i];
             }
-
-
             let workDays = +mshifttypeone.value;
             let weekendDays = +mshifttypetwo.value;
             let shiftWorkDays = workDays + weekendDays;
 
-            for (let i = startDay; i < collection().length; i = i + shiftWorkDays) {
+            for (let i = startDay; i < collectionForLeap().length; i = i + shiftWorkDays) {
                 for (let j = i; j < workDays + i; j++) {
-                    if (j < collection().length)
-                        collection()[j].classList.add('menu_butcol');
+                    if (j < collectionForLeap().length)
+                    collectionForLeap()[j].classList.add('menu_butcol');
                 }
             }
-            //console.log(collection());
         }
-
-
-    } else if (+menuyear.value == 0) {
-        /*for (let i = 0; i < datey.length; i++) {
-            datey[59].remove();
-        }*/
-        collection()[59].remove();
-        //console.log(collection());
-        // weekleap1.innerHTML = 'ПН';
-        //weekleap2.innerHTML = 'ВС';
+    } else if (+menusyear.value == 0) {
+        collectionForLeap()[59].remove();
     }
 };
-
- //console.log(date);
-
-
-
-
-//console.log(datey);
